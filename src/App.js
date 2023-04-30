21cc9d00ba60b7626991bf51a83c1febc04eadbf
zlatan-cic <smajicz999@gmail.com>
2023-04-30 08:34:22 +0200

    geting ready for css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
-  
+  const [role, setRole] = useState('dev');
   const showEmployee = true;
   return (
     <div className="App">
       
       {showEmployee ?
         <>
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
-          <Employee />
+        <input type='text' 
+        onChange={(e) => {
+          console.log(e.target.value);
+          setRole(e.target.value);
+        }}></input>
+          <Employee name='Bob' role='Intern'/>
+          <Employee name='Aby' role={role}/>
+          <Employee name='Vladimir' />
         </>
         :
         <p>You cannot see the employee</p>
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,5 +1,14 @@
-function Employee(){
-  return <h3>Here is a employee</h3>
+function Employee(props){
+  
+  return (
+    <>
+      <h3>Employee {props.name}</h3>
+      <p>{props.role ? props.role : "No Role"}</p>
+      {/* {props.role ? <p>{props.role}</p> : <p>No Role</p>} */}
+
+    </>
+
+  )
 }
 
 export default Employee;
